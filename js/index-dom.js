31157690e136250da6159d19015f2736c0c935cc
Gonzalo <gon.pineiro@gmail.com>
2020-12-04 12:21:56 -0300

    Reloj

--- js/index-dom.js
+++ js/index-dom.js
@@ -1,8 +1,13 @@
 import hamburgerMenu from './dom/menu-hamburguesa.js';
+import {alarm, digitalClock} from './dom/reloj.js'
 
 const d = document;
 
-/* Evento del menu hamburguesa */
 d.addEventListener('DOMContentLoaded', (e) => {
+    /* Evento del menu hamburguesa */
     hamburgerMenu('.panel-btn', '.panel', '.menu a');
+
+    /* Evento del Reloj */
+    digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj');
 });
+
